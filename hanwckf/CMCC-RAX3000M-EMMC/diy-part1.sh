f9e2682a2a0ff7b739101d3f17d7b49239e11b91
xjqxziii <xjqxziii@163.com>
2023-09-25 23:17:51 +0800

    Update .config

--- hanwckf/CMCC-RAX3000M-EMMC/diy-part1.sh
+++ hanwckf/CMCC-RAX3000M-EMMC/diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
-echo 'src-git ZeaKyX https://github.com/ZeaKyX/luci-app-speedtest-web' >>feeds.conf.default
+echo 'src-git netspeedtest https://github.com/muink/luci-app-netspeedtest.git' >>feeds.conf.default
