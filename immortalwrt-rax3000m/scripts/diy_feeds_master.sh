fe1de5c0e87652918d0aa7c2997b2e2aecdb30d4
xjqxziii <xjqxziii@163.com>
2023-10-07 14:50:04 +0800

    Update master

---
+++ immortalwrt-rax3000m/scripts/diy_feeds_master.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
